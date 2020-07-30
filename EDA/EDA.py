4abb03d354e16e9c3a05c59da3d30cae14fda41d
Lee_JunWon <cpprhtn@naver.com>
2020-07-30 19:56:57 +0900

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -7,38 +7,170 @@ Created on Tue Jul 28 14:28:28 2020
 """
 
 import pandas as pd
-# 최대 줄 수 설정
+import math
 pd.set_option('display.max_rows', 10000)
-# 최대 열 수 설정
 pd.set_option('display.max_columns', 10000)
-# 표시할 가로의 길이
 pd.set_option('display.width', 10000)
 
-busan_08 = pd.read_csv('busan_08.csv',  encoding = 'cp949')
-seoul_08 = pd.read_csv('seoul_08.csv',  encoding = 'cp949')
 
-busan_08.columns
+'''
+10~13년까지는 fma_12z1, fma_20z1을 이용하여 
+14~ 의 형식에 맞도록 정제할 예정
 
-
-
-
-'bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
-'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
-'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1'
+1  50미만
+2  ~100미만
+3  ~200미만
+4  ~300미만
+5  ~400미만
+6  ~500미만
+7  ~600미만
+8  600이상
+77 응답거부
+99 모름
+'''
 
 
 busan_10 = pd.read_csv('busan_10.csv',  encoding = 'cp949')
-seoul_10 = pd.read_csv('seoul_10.csv',  encoding = 'cp949')
 
-b_10 = busan_10[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
-                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
-                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
-s_10 = seoul_10[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
-                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
-                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+b_10 = busan_10[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','fma_20z1',
+                 'sma_03z2','drb_01z2','pha_04z1','pha_07z1','phb_01z1','nua_01z1',
+                 'nub_01z1','oba_01z1','obb_01z1','oba_02z1','oba_03z1','mtc_01z1',
+                 'mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
 
 b_10.isna().sum()
-s_10.isna().sum()
 
+idx_nm = b_10[b_10['fma_12z1']!=2].index
+b_10 = b_10.drop(idx_nm)
+b_10.reset_index(inplace = True)
+
+
+#14년 이후의 형식으로 변환
+b_10['fma_20z1'] = b_10['fma_20z1']/100 + 2
+def kkk(x):
+    if x<2.5: return 1
+    elif x<3: return 2
+    elif x>9: return 9
+    else: return math.trunc(x)
+        
+b_10['fma_20z1'] = b_10['fma_20z1'].apply(kkk)
+
+
+b_10.rename(columns = {'fma_20z1' : 'fma_24z1'}, inplace = True)
+del b_10['index']
+del b_10['fma_12z1']
 b_10.to_csv('b_10.csv', index=False)
+
+
+
+
+
+
+seoul_10 = pd.read_csv('seoul_10.csv',  encoding = 'cp949')
+s_10 = seoul_10[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','fma_20z1',
+                 'sma_03z2','drb_01z2','pha_04z1','pha_07z1','phb_01z1','nua_01z1',
+                 'nub_01z1','oba_01z1','obb_01z1','oba_02z1','oba_03z1','mtc_01z1',
+                 'mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+
+s_10.isna().sum()
+#s_10의 결측치 제거
+s_10.dropna(inplace = True)
+
+idx_nm = s_10[s_10['fma_12z1']!=2].index
+s_10 = s_10.drop(idx_nm)
+s_10.reset_index(inplace = True)
+
+s_10['fma_20z1'] = s_10['fma_20z1']/100 + 2
+        
+s_10['fma_20z1'] = s_10['fma_20z1'].apply(kkk)
+
+s_10.rename(columns = {'fma_20z1' : 'fma_24z1'}, inplace = True)
+del s_10['index']
+del s_10['fma_12z1']
 s_10.to_csv('s_10.csv', index=False)
+
+
+
+
+
+
+
+
+busan_11 = pd.read_csv('busan_11.csv',  encoding = 'cp949')
+seoul_11 = pd.read_csv('seoul_11.csv',  encoding = 'cp949')
+
+b_11 = busan_11[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+s_11 = seoul_11[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+
+b_11.isna().sum()
+s_11.isna().sum()
+
+b_11.to_csv('b_11.csv', index=False)
+s_11.to_csv('s_11.csv', index=False)
+
+
+
+busan_12 = pd.read_csv('busan_12.csv',  encoding = 'cp949')
+seoul_12 = pd.read_csv('seoul_12.csv',  encoding = 'cp949')
+
+b_12 = busan_12[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+s_12 = seoul_12[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+
+b_12.isna().sum()
+s_12.isna().sum()
+
+b_12.to_csv('b_12.csv', index=False)
+s_12.to_csv('s_12.csv', index=False)
+
+
+
+
+
+
+busan_13 = pd.read_csv('busan_13.csv',  encoding = 'cp949')
+seoul_13 = pd.read_csv('seoul_13.csv',  encoding = 'cp949')
+
+b_13 = busan_13[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+s_13 = seoul_13[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z1']]
+
+b_13.isna().sum()
+s_13.isna().sum()
+
+b_13.to_csv('b_13.csv', index=False)
+s_13.to_csv('s_13.csv', index=False)
+
+
+
+
+
+
+#14년부터는 fma_24z1 이용(월 소득)
+busan_14 = pd.read_csv('busan_14.csv',  encoding = 'cp949')
+seoul_14 = pd.read_csv('seoul_14.csv',  encoding = 'cp949')
+
+b_14 = busan_14[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_24z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z2']]
+s_14 = seoul_14[['bogun_cd','age','sex','apt_t','fma_19z1','fma_04z1','fma_12z1','sma_03z2','drb_01z2',
+                 'pha_04z1','pha_07z1','phb_01z1','nua_01z1','nub_01z1','oba_01z1','obb_01z1','oba_02z1',
+                 'oba_03z1','mtc_01z1','mta_01z1','mtb_01z1','hya_06z1','dia_06z1','ara_22z1','sod_02z2']]
+
+b_14.isna().sum()
+s_14.isna().sum()
+
+b_14.to_csv('b_14.csv', index=False)
+s_14.to_csv('s_14.csv', index=False)
+
+
+
