180d2472038b2dde1cb9750a3e8482027e5cf32d
Lee_JunWon <cpprhtn@naver.com>
2020-08-10 21:07:16 +0900

    8/10

--- Predict_Model/Decision_Tree/Decision_Tree_Model.R
+++ Predict_Model/Decision_Tree/Decision_Tree_Model.R
@@ -19,7 +19,7 @@ dt_busan = rbind(b_10,b_11,b_12,b_13,b_14,b_15,b_16,b_17)
 
 #다항의 decision tree model로 예층하기위해서 종속변수의 범주는 string format를 주기로 함
 #1=G_underweight, 2=G_normal, 3=G_obesity
-
+'''
 dt_busan[,"Multinomial"] <- 'G_obesity'
 for (i in 1:109314){
   if (dt_busan$Obesity[i] == 1){
@@ -29,6 +29,8 @@ for (i in 1:109314){
     dt_busan$Multinomial[i] <- 'G_normal'
   }
 }
+'''
+dt_busan[,"Multinomial"] <- dt_busan$Obesity
 str(dt_busan$Multinomial)
 read.csv("Predict_Model/input_columns.csv",header = T, sep = ',') -> input
 read.csv("Predict_Model/output_multinomial.csv",header = T,sep=',') -> output
