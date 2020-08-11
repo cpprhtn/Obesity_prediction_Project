3647718b5cdb09a29e4db02cd74e798901c80c7d
Lee_JunWon <cpprhtn@naver.com>
2020-08-11 20:21:13 +0900

    8/11

--- Predict_Model/Random_Forest/Random_Forest_Model.R
+++ Predict_Model/Random_Forest/Random_Forest_Model.R
@@ -10,8 +10,13 @@ read.csv("Predict_Model/Random_Forest/pre_output_rf.csv",header = T,sep=',') -> 
 input_value = c(colnames(input))
 output_value = c(colnames(output))
 pre_value = c(colnames(pre_output))
-form = as.formula(paste(paste(output_value, collapse = '+'),'~',paste(input_value, collapse = '+')))
 
+str(output_value)
+output_value <- as.character(output_value)
+output_value <- as.factor(output_value)
+
+form = as.formula(paste(paste(output_value, collapse = '+'),'~',paste(input_value, collapse = '+')))
+form
 read.csv("EDA/종속변수생성/b_10.csv",header = T,fileEncoding = "CP949") -> b_10
 read.csv("EDA/종속변수생성/b_11.csv",header = T,fileEncoding = "CP949") -> b_11
 read.csv("EDA/종속변수생성/b_12.csv",header = T,fileEncoding = "CP949") -> b_12
@@ -21,7 +26,7 @@ read.csv("EDA/종속변수생성/b_15.csv",header = T,fileEncoding = "CP949") ->
 read.csv("EDA/종속변수생성/b_16.csv",header = T,fileEncoding = "CP949") -> b_16
 read.csv("EDA/종속변수생성/b_17.csv",header = T,fileEncoding = "CP949") -> b_17
 
-busan = rbind(b_10,b_11,b_12,b_13)#),b_14,b_15,b_16,b_17)
+busan = rbind(b_10,b_11)#),b_12,b_13,b_14,b_15,b_16,b_17)
 str(rf_busan)
 for (i in 1:24) {
   rf_busan[,i] <-factor(rf_busan[,i]) 
@@ -30,5 +35,10 @@ form
 busan.rf = randomForest(form, data= busan, forest=FALSE, importance=TRUE)
 
 pre = predict(busan.rf,busan, type='prob')
+dimnames(pre)=list(NULL,c(pre_value))
+summary(pre)
+
+b_10.rf = randomForest(form, data = b_10, forest=FALSE, importance=TRUE)
+pre = predict(b_10.rf,b_10, type='prob')
 dimnames(pre)=list(NULL,c(pre_value))
 summary(pre)
